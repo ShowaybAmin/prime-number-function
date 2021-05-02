dd487b5f9e96ce82f938cfe1c23f1e730d05133b
ShowaybAmin <83536259+ShowaybAmin@users.noreply.github.com>
2021-05-02 16:30:59 +0600

    Create prime-number.py

---
+++ prime-number.py
@@ -0,0 +1,32 @@
+a = []
+
+b = int(input("Last number to search for prime number: "))
+
+def _solution_():
+
+	flag = True
+
+	for i in range(2, p):
+
+		if (p % i) == 0:
+
+			flag = False
+
+			break
+
+	if flag:
+
+		a.append(p)
+
+if b < 2:
+
+	print("Sorry...The first prime number is 2")
+
+else:
+
+	for p in range(2, b + 1):
+
+		_solution_()
+
+print(a)
+
